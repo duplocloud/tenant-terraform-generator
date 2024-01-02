d052a82a324333901de649cfc8d6c2324e6ef6a5
Andy Boutte <andyboutte@gmail.com>
2024-01-02 16:18:05 -0600

    DUPLO-13662 - Calculate default tenant id and aws account id (#40)

--- scripts/_env.sh
+++ scripts/_env.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -eu
 
-AWS_ACCOUNT_ID="$aws_account_id"
-duplo_default_tenant_id="$tenant_id"
+# Discover both the AWS Account ID and DuploCloud Default Tenant ID if they are not set.
+# Historically the documentation has had the user set these values so allow the user to specifiy the values and use the user provided values.
+# To support both commercial and GovCloud regions, first grab the region of the default infrastructure which can then be used in `with_aws`
+_VALID_AWS_REGION=$(duplo_api /adminproxy/GetInfrastructureConfig/default | jq -r '.Region')
+AWS_ACCOUNT_ID=$([ -z "${aws_account_id:-}" ] && with_aws aws --region "$_VALID_AWS_REGION" sts get-caller-identity | jq -r '.Account' || echo "$aws_account_id")
+duplo_default_tenant_id=$([ -z "${tenant_id:-}" ] && duplo_api /adminproxy/GetTenantNames | jq -r '.[] | select(.AccountName == "default") | .TenantId' || echo "$tenant_id")
+
 duplo_host="$duplo_host"
 duplo_token="$duplo_token"
 
